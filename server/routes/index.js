c1efad21dd97f0d8f48df806b20b010a1a2a9b66
Luis <delapenl@gmail.com>
2015-08-21 15:45:37 -0600

    add files

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Add A Puppy!' });
 });
+
+router.get('/puppies/new', function(req, res, next) {
+  res.render('puppiesnew');
+});
+
+router.post('/puppies', function(req, res, next) {
+   res.render('puppytable', {
+    name:req.body.text1,
+    id:req.body.text2
+   });
+});
+
 
 module.exports = router;
